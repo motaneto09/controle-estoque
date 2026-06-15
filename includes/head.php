0109f95e4e2de84f3e8a5916a23bd6fa4f9974b3
Deoclécio Mota <deoclecioneto642@gmail.com>
2026-06-15 14:16:13 -0300

    Ajuste dos caminhos do CSS para caminhos relativos

--- includes/head.php
+++ includes/head.php
@@ -1,10 +1,7 @@
 <meta charset="UTF-8">
 
-<link rel="stylesheet" href="/controle-estoque/assets/css/style.css">
+<link rel="stylesheet" href="assets/css/style.css">
 
-<link rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-<link rel="icon"
-      type="image/png"
-      href="/controle-estoque/assets/img/favicon.png?v=2">
+<link rel="icon" type="image/png" href="assets/img/favicon.png?v=2">
